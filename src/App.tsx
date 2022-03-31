3b90d0ccfa6bf4f86e7d392252b7242463303788
yu2799 <ura.yu.2799@gmail.com>
2022-03-31 19:53:36 +0900

    muiを使って見た目を整形した。マークダウンパーサを導入した。

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, Grid, Paper } from "@mui/material";
+import { VFC } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-function App() {
+const App: VFC = () => {
+  type documentObject = {
+    id: string;
+    url: string;
+    markdownContent: {
+      lead?: string;
+      procedure?: string;
+      question: string;
+    };
+    options: { label: string; next: string }[];
+  };
+
+  const leadMarkdown: string = `# Hello World
+  ## hoge
+  - a
+  - *b*
+    - **c**
+  `;
+
+  /*
+    remark-gfmを使うと
+    ・取り消し線
+    ・テーブル
+    ・URL
+    ・タスクリスト
+    が使える
+  */
+  const procedureMarkdown: string = `Just a link: https://reactjs.com.
+
+  ~~取り消し線~~
+  * [ ] hoge
+  * [x] piyo
+  `;
+
+  const questionMarkdown: string = `\`aiueo\`
+
+  aiueo`;
+
+  const documentObject: documentObject = {
+    id: "1",
+    url: "2",
+    markdownContent: {
+      lead: leadMarkdown,
+      procedure: procedureMarkdown,
+      question: questionMarkdown,
+    },
+    options: [
+      {
+        label: "a",
+        next: "2",
+      },
+    ],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ backgroundColor: "#f5f5f5", py: 3 }}>
+      <Container>
+        <Grid container rowSpacing={2}>
+          {Object.entries(documentObject.markdownContent).map(
+            ([key, data]): JSX.Element => {
+              return (
+                <Grid item xs={12} key={key}>
+                  <Paper elevation={5} sx={{ p: 2 }}>
+                    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                      {data}
+                    </ReactMarkdown>
+                  </Paper>
+                </Grid>
+              );
+            }
+          )}
+        </Grid>
+      </Container>
+    </Box>
   );
-}
+};
 
 export default App;
