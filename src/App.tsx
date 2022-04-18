06b7e3770077bbe60df19570d82c1a4f2ce0ca5a
yu2799 <ura.yu.2799@gmail.com>
2022-04-18 14:52:39 +0900

    documentの遷移を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,77 @@
-import { Box, Container, Grid, Paper } from "@mui/material";
-import { VFC } from "react";
+import { Box, Container, Grid, Paper, Button } from "@mui/material";
+import { useEffect, useState, VFC } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import getDocument from "./api/api";
 
 const App: VFC = () => {
   type documentObject = {
+    title: string;
     id: string;
     url: string;
     markdownContent: {
-      lead?: string;
-      procedure?: string;
-      question: string;
+      lead?: string[];
+      procedure?: string[];
+      question: string[];
     };
     options: { label: string; next: string }[];
   };
 
-  const leadMarkdown: string = `# Hello World
-  ## hoge
-  - a
-  - *b*
-    - **c**
-  `;
+  const [content, setContent] = useState<documentObject | null>(null);
 
-  /*
-    remark-gfmを使うと
-    ・取り消し線
-    ・テーブル
-    ・URL
-    ・タスクリスト
-    が使える
-  */
-  const procedureMarkdown: string = `Just a link: https://reactjs.com.
-
-  ~~取り消し線~~
-  * [ ] hoge
-  * [x] piyo
-  `;
-
-  const questionMarkdown: string = `\`aiueo\`
-
-  aiueo`;
-
-  const documentObject: documentObject = {
-    id: "1",
-    url: "2",
-    markdownContent: {
-      lead: leadMarkdown,
-      procedure: procedureMarkdown,
-      question: questionMarkdown,
-    },
-    options: [
-      {
-        label: "a",
-        next: "2",
-      },
-    ],
+  const handleDocumentChange = (next: string) => {
+    getDocument(next).then((document) => {
+      setContent(document);
+    });
   };
+
+  useEffect(() => {
+    getDocument("1").then((data: documentObject) => {
+      setContent(data);
+    });
+  }, []);
+
+  if (content === null) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <Box sx={{ backgroundColor: "#f5f5f5", py: 3 }}>
       <Container>
         <Grid container rowSpacing={2}>
-          {Object.entries(documentObject.markdownContent).map(
+          {Object.entries(content.markdownContent).map(
             ([key, data]): JSX.Element => {
               return (
                 <Grid item xs={12} key={key}>
                   <Paper elevation={5} sx={{ p: 2 }}>
                     <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                      {data}
+                      {data.join("\n")}
                     </ReactMarkdown>
+                    {key === "question" && (
+                      <Grid container columnSpacing={2}>
+                        {content.options.map(({ label, next }, key) => {
+                          return (
+                            <Grid
+                              item
+                              xs={4}
+                              key={key}
+                              sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                              }}
+                            >
+                              <Button
+                                variant="contained"
+                                onClick={() => handleDocumentChange(next)}
+                              >
+                                {label}
+                              </Button>
+                            </Grid>
+                          );
+                        })}
+                      </Grid>
+                    )}
                   </Paper>
                 </Grid>
               );
