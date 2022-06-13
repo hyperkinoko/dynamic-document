e724ccb788408ef606491a4c1c4a79677215d2de
yu2799 <ura.yu.2799@gmail.com>
2022-06-13 13:32:41 +0900

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { VFC } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 
 export const App: VFC = () => {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
