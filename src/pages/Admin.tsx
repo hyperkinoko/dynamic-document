883a8b072954d109b833f3eb9c7fdfcad3bbba90
yu2799 <ura.yu.2799@gmail.com>
2022-06-30 10:53:04 +0900

    fix

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,5 +1,64 @@
+import { Box, Grid, Paper, Typography } from "@mui/material";
 import { VFC } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Admin: VFC = () => {
-  return <p>ﾅｲﾖｰ</p>;
+  const nav = useNavigate();
+
+  return (
+    <Box
+      sx={{
+        p: 4,
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translateY(-50%) translateX(-50%)",
+      }}
+    >
+      <Grid container columnSpacing={5}>
+        <Grid
+          item
+          xs={6}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Paper
+            elevation={5}
+            sx={{ p: 2 }}
+            onClick={() => {
+              nav("/view");
+            }}
+          >
+            <Typography variant={"h5"} sx={{ p: 2 }}>
+              ドキュメントを閲覧
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Paper
+            elevation={5}
+            sx={{ p: 2 }}
+            onClick={() => {
+              nav("/edit");
+            }}
+          >
+            <Typography variant={"h5"} sx={{ p: 2 }}>
+              ドキュメントを編集
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
