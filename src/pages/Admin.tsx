5da5fc376783fbeaf801044b6640bf62f08e9a26
yu2799 <ura.yu.2799@gmail.com>
2022-08-15 21:22:47 +0900

    add to search for first document

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,9 +1,62 @@
-import { Box, Grid, Paper, Typography } from "@mui/material";
-import { VFC } from "react";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Grid,
+  Paper,
+  Typography,
+  TextField,
+  List,
+  ListItemButton,
+  ListItemText,
+  Divider,
+} from "@mui/material";
+import { FC, useState, useEffect, Fragment } from "react";
 import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { allDocumentsState } from "../atoms/AllDocumentsData";
+import { getAllDocuments } from "../api/api";
+import { titleSetsType } from "../types/titleSetsType";
 
-export const Admin: VFC = () => {
+export const Admin: FC = (): JSX.Element => {
   const nav = useNavigate();
+  const [open, setOpen] = useState<boolean>(false);
+  const [buf, setBuf] = useState<string>("");
+  const [titleSets, setTitleSets] =
+    useRecoilState<titleSetsType>(allDocumentsState);
+
+  const onClose = (): void => {
+    setOpen(false);
+    setBuf("");
+  };
+
+  const handleViewClick = (): void => {
+    if (buf === "") {
+      alert("閲覧するドキュメントのタイトルを入力してください");
+    } else {
+      for (const [id, title] of Object.entries(titleSets)) {
+        if (buf === title) {
+          nav("/view", { state: id });
+          return;
+        }
+      }
+      alert("存在すするドキュメントのタイトルを入力してください");
+    }
+  };
+
+  useEffect(() => {
+    getAllDocuments("documents").then((data) => {
+      const res: { [key: string]: string } = { 未定: "未定" };
+      for (const doc of data) {
+        const title: string = doc.title;
+        const id: string = doc.id;
+        res[id] = title;
+      }
+      setTitleSets(res);
+    });
+  }, []);
 
   return (
     <Box
@@ -29,7 +82,7 @@ export const Admin: VFC = () => {
             elevation={5}
             sx={{ p: 2 }}
             onClick={() => {
-              nav("/view", { state: "fLlvvfjKSr5sYq4vNHZz" });
+              setOpen(true);
             }}
           >
             <Typography variant={"h5"} sx={{ p: 2 }}>
@@ -59,6 +112,43 @@ export const Admin: VFC = () => {
           </Paper>
         </Grid>
       </Grid>
+      <Dialog open={open} onClose={onClose}>
+        <DialogContent>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            label="閲覧するドキュメントのタイトル"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setBuf(e.target.value)}
+            value={buf}
+            autoComplete="off"
+          />
+          <List>
+            {Object.entries(titleSets)
+              .filter(([_, name]) => name !== "未定")
+              .map(([key, name], idx) => {
+                return (
+                  <Fragment key={key}>
+                    <ListItemButton onClick={() => setBuf(name)}>
+                      <ListItemText>{name}</ListItemText>
+                    </ListItemButton>
+                    <Divider />
+                  </Fragment>
+                );
+              })}
+          </List>
+        </DialogContent>
+        <DialogActions sx={{ p: 1 }}>
+          <Button variant="contained" color="error" onClick={onClose}>
+            Cancel
+          </Button>
+          <Button variant="contained" onClick={handleViewClick}>
+            View
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
