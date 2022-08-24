96d64e6e52e625c42d1f96c304c57b7d04e9a2b8
yu2799 <ura.yu.2799@gmail.com>
2022-08-24 18:01:11 +0900

    update react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById("root");
+// typescripst環境だとcontainer要素がない場合がある旨のエラーが出るので存在しない場合はErrorをthrowさせておく
+if (!container) throw new Error("Failed to find the root element");
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
     <RecoilRoot>
       <App />
     </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
